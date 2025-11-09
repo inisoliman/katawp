4f8ca52f42759deb7fd1d70de3f855698fa46ac6
inisoliman <one4vc@gmail.com>
2025-11-09 02:20:53 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,17 +1,34 @@
 <?php
-// دوال مساعدة للتعامل مع جداول البيانات وعرض القراءات
-function katawp_get_reading($date) {
-    global $wpdb;
-    // مثال: جلب قراءة معينة بناء على التاريخ
-    $table = 'katamars_readings';
-    $row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table WHERE read_date=%s", $date));
-    return $row;
+
+if (!defined('ABSPATH')) exit;
+
+function katawp_get_today_reading() {
+    $db = new KataWP_Database();
+    return $db->get_today_reading();
 }
 
-function katawp_get_synax($date) {
-    global $wpdb;
-    $table = 'katamars_synax';
-    return $wpdb->get_row($wpdb->prepare("SELECT * FROM $table WHERE synax_date=%s", $date));
+function katawp_get_reading_by_date($date) {
+    $db = new KataWP_Database();
+    return $db->get_today_reading($date);
 }
 
-// إضافة دوال لمزيد من العمليات حسب قاعدة البيانات الأصلية
+function katawp_search_readings($query) {
+    $db = new KataWP_Database();
+    return $db->search_readings($query);
+}
+
+function katawp_format_date($gregorian_date) {
+    $date = new DateTime($gregorian_date);
+    return $date->format('d/m/Y');
+}
+
+function katawp_get_coptic_date($date) {
+    // تحويل التاريخ الميلادي إلى القبطي
+    $reading = new KataWP_Database();
+    $result = $reading->wpdb->get_row(
+        "SELECT coptic_day, coptic_month FROM " . KATAWP_DB_PREFIX . "daily_readings 
+        WHERE gregorian_date = '" . $date . "'"
+    );
+    
+    return $result ? $result->coptic_day . ' ' . $result->coptic_month : '';
+}
