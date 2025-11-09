b884a57f35878d8944be5f990144217b924101bc
inisoliman <one4vc@gmail.com>
2025-11-09 02:21:12 +0200

    Update shortcode.php

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -1,22 +1,54 @@
 <?php
-// كود شورت كود لعرض القراءة اليومية
-function katawp_readings_shortcode($atts) {
-    $atts = shortcode_atts([
-        'date' => date('Y-m-d')
-    ], $atts);
-    $reading = katawp_get_reading($atts['date']);
-    $synax = katawp_get_synax($atts['date']);
-    ob_start();
-    echo '<div class="katawp-reading">';
-    if ($reading) {
-        echo '<h3>القراءات اليومية:</h3>';
-        echo '<div>' . esc_html($reading->read_content) . '</div>';
+
+if (!defined('ABSPATH')) exit;
+
+class KataWP_Shortcodes {
+    public function __construct() {
+        add_shortcode('katawp_readings', [$this, 'readings_shortcode']);
+        add_shortcode('katawp_search', [$this, 'search_shortcode']);
+        add_shortcode('katawp_saint', [$this, 'saint_shortcode']);
     }
-    if ($synax) {
-        echo '<h4>السنكسار:</h4>';
-        echo '<div>' . esc_html($synax->synax_content) . '</div>';
+    
+    public function readings_shortcode($atts) {
+        $atts = shortcode_atts(['date' => date('Y-m-d')], $atts);
+        
+        $db = new KataWP_Database();
+        $reading = $db->get_today_reading($atts['date']);
+        
+        if (!$reading) {
+            return '<p>لا توجد قراءات لهذا اليوم</p>';
+        }
+        
+        ob_start();
+        include KATAWP_PLUGIN_DIR . 'templates/daily-readings.php';
+        return ob_get_clean();
     }
-    echo '</div>';
-    return ob_get_clean();
+    
+    public function search_shortcode($atts) {
+        ob_start();
+        include KATAWP_PLUGIN_DIR . 'templates/search.php';
+        return ob_get_clean();
+    }
+    
+    public function saint_shortcode($atts) {
+        $atts = shortcode_atts(['name' => ''], $atts);
+        
+        if (empty($atts['name'])) {
+            return '';
+        }
+        
+        global $wpdb;
+        $saint = $wpdb->get_row(
+            "SELECT * FROM " . KATAWP_DB_PREFIX . "synaxarium 
+            WHERE saint_name LIKE '%" . $wpdb->esc_like($atts['name']) . "%'"
+        );
+        
+        if (!$saint) {
+            return '<p>لم يتم العثور على القديس</p>';
+        }
+        
+        return '<div class="katawp-saint"><h3>' . $saint->saint_name . '</h3><p>' . $saint->biography . '</p></div>';
+    }
 }
-add_shortcode('katawp_readings', 'katawp_readings_shortcode');
+
+new KataWP_Shortcodes();
